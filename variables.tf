ea9b63bfcfdaec6d7769ef5a96a57613661a1b8b
Opsmotion <36114481+opsmotion@users.noreply.github.com>
2018-02-07 09:47:57 +0000

    added default port variable

--- variables.tf
+++ variables.tf
@@ -3,3 +3,7 @@ variable "secret_key" {}
 variable "region" {
     default = "eu-west-1"
 }
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 80
+}
