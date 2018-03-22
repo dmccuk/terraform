e81c8d952ca8ab0067c101a914f84e9b0a940200
Opsmotion <36114481+opsmotion@users.noreply.github.com>
2018-03-22 17:30:13 +0000

    Updates to code and readme.md (#2)
    * Update README.md
    
    * Update README.md
    
    * added a security group to the code
    
    * Added in openscap
    
    * Update scripts.sh
    
    * added default port variable
    
    * Update main.tf
    
    * Update scripts.sh
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Add files via upload
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- main.tf
+++ main.tf
@@ -4,12 +4,39 @@ provider "aws" {
     region = "${var.region}"
 }
 
+resource "aws_security_group" "ssh_web" {
+  name = "terraform-fw"
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Inbound ssh from anywhere
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Outbound to anywhere
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
-    ami             = "ami-f9dd458a"
+    ami             = "ami-c90195b0" #Red Hat 7.4
     instance_type   = "t2.micro"
     tags { Name     = "Terraform-test" }
-    security_groups = ["CHANGE_ME"]
-    key_name        = "CHANGE_ME"
+    security_groups = ["terraform-fw"]
+    # key_name is your AWS keypair to allow you access
+    key_name        = "Add_Your_Keypair_name"
     provisioner "file" {
         source      = "files/scripts.sh"
         destination = "/tmp/scripts.sh"
@@ -21,10 +48,11 @@ resource "aws_instance" "web" {
           "sudo /tmp/scripts.sh"
         ]
     }
+    # Setup user access via SSH
     connection {
       type = "ssh"
       user = "ec2-user"
-      private_key = "${file("VALUE.pem")}"
+      private_key = "${file("/location/of/your/.pem")}"
       timeout = "2m"
       agent = false
     }
